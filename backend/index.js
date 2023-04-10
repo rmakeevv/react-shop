cc34f6e8bb2b4f7a2e1d3e1aef52b632f045d834
rmakeevv <rmakeev.gm@gmail.com>
2023-04-10 17:54:04 +0400

    production

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const client = require('./db')
 const bodyParser = require('body-parser')
@@ -7,7 +6,6 @@ const router = require('./routes/router')
 const cors = require('cors')
 require('dotenv').config()
 
-app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json())
 app.use(cors())
 app.use('/', router)
