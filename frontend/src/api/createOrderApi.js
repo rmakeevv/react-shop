53121a3132b60d21b055654c745f83f1f75569ef
rmakeevv <rmakeev.gm@gmail.com>
2023-04-10 22:59:37 +0400

    production

--- frontend/src/api/createOrderApi.js
+++ frontend/src/api/createOrderApi.js
@@ -1,6 +1,6 @@
 import {redirect} from "react-router-dom";
 export const createOrderApi = async basketData => {
-    const data = await fetch(`http://51.250.65.58/:5000/orders/`, {
+    const data = await fetch(`http://51.250.65.58:5000/orders/`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json;charset=utf-8'
