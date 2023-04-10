fbc4d402291ba657a7eab300f02deb6928281a25
rmakeevv <rmakeev.gm@gmail.com>
2023-04-10 19:18:40 +0400

    production

--- frontend/src/api/getOneProduct.js
+++ frontend/src/api/getOneProduct.js
@@ -1,5 +1,5 @@
 export const getOneProduct = async({params}) => {
-    const data = await fetch(`http://localhost:5000/products/${params.id}`)
+    const data = await fetch(`http://51.250.65.58:5000/products/${params.id}`)
         .then(res => res.json())
         .then(data => data)
         .catch((err) => console.log(err.message))
--- frontend/src/services/getImage.js
+++ frontend/src/services/getImage.js
@@ -1,1 +1,1 @@
-export const getImage = (src) => `http://localhost:5000/images/${src}`
+export const getImage = (src) => `http://51.250.65.58:5000/images/${src}`
