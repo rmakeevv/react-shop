ee5bbb2f0c3faa0c2c68d81ce034617abd56b6c7
rmakeevv <rmakeev.gm@gmail.com>
2023-04-11 16:39:27 +0400

    development release

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const password = encodeURIComponent("EFihZ76nDYEaqMQy")
 const uri = `mongodb+srv://rmakeevv:${password}@cluster0.g2wn3u9.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//     const collection = client.db("test")
-//     // perform actions on the collection object
-//     client.close();
-// });
+const client = new MongoClient(uri,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverApi: ServerApiVersion.v1
+        }
+    );
 
 module.exports = client
