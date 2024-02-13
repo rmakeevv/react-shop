1286c46468555f97a4e4c222db5d3b6746a86615
rmakee <r.makeevv@gmail.com>
2024-02-13 04:04:13 +0400

    upd

--- frontend/src/routes/Auth.jsx
+++ frontend/src/routes/Auth.jsx
@@ -5,13 +5,15 @@ import { MyLink } from '../components/MyLink';
 export const Auth = () => {
   const auth = useSelector((state) => state.auth.value);
   const errors = useActionData();
-  return auth.isLogged ? (
+  if (auth.isLogged) return (
     <div className={'grid text-white justify-items-center gap-4'}>
       <h1 className={'md:text-3xl p-4'}>Awesome!</h1>
       <span className={'text-2xl'}>You're logged in!</span>
       <MyLink href={'/products'} text={'go to store'} />
     </div>
-  ) : (
+  ) 
+
+  return (
     <div
       className={'grid gap-4 justify-items-center items-center bg-sky-200 py-4'}
     >
