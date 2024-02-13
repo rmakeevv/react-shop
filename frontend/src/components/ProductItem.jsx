16926a0ca825f2c1e817429732c273aae471ac6a
rmakee <r.makeevv@gmail.com>
2024-02-13 04:09:41 +0400

    style

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -1,21 +1,23 @@
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
+import { useMemo } from 'react';
 export const ProductItem = ({ name, brand, price, _id, action, img }) => {
-  const picture = `http://localhost:5000/images/${img}`;
+  const picture = useMemo(() => `http://localhost:5000/images/${img}`, [img]);
+
   return (
-    <div className={'md:flex bg-slate-900 p-8 md:rounded-md items-center'}>
+    <div className={'md:flex bg-slate-900 p-8 md:rounded-md items-center '}>
       <Link to={`/products/${_id}`}>
         <img src={picture} alt={name} width={'330px'} />
       </Link>
-      <div className={'flex flex-col items-center'}>
-        <div className={'m-4 flex items-center'}>
+      <div className={'flex flex-col gap-4'}>
+        <div className={'flex items-center'}>
           <Link to={`/products/${_id}`}>{brand + ' ' + name} </Link>
           <Link to={`/products/${_id}`} className={'p-4'}>
             Подробнее
           </Link>
         </div>
         <div className={'flex items-center gap-4'}>
-          <span className={'p-4'}>Товар в наличии!</span>
+          <span className={''}>Товар в наличии!</span>
           <p className={'text-lg'}>{price + ' руб.'}</p>
           <Button action={action} text={'Добавить в корзину'} />
         </div>
